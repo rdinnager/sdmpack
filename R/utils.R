b83f142200d1b95c5f386d8fad5e6c63b49c653b
Russell Dinnage <rdinnage@fiu.edu>
2023-10-03 17:03:50 -0400

    fixed up lectures and added util function

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,26 @@ run_exam_1 <- function() {
   learnr::run_tutorial(local_file)
 }
 
+## function borrowed from package {TeachingDemos} function char2seed(): https://cran.r-project.org/web/packages/TeachingDemos/index.html
+
+#' @export
+set.seed.by.name <- function(x, set = TRUE, ...){
+
+	tmp <- c(0:9,0:25,0:25)
+	names(tmp) <- c(0:9,letters,LETTERS)
+
+	x <- gsub("[^0-9a-zA-Z]","",as.character(x))
+
+	xsplit <- tmp[ strsplit(x,'')[[1]] ]
+
+	seed <- sum(rev( 7^(seq(along=xsplit)-1) ) * xsplit)
+        seed <- as.integer( seed %% (2^31-1) )
+
+	if(set){
+		set.seed(seed,...)
+		return(invisible(seed))
+	} else {
+		return(seed)
+	}
+}
+
